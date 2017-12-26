66c30d7adb6dd7dcb3904641fe3f157e1c62f629
Miau <tankorn@wp.pl>
2017-12-26 13:54:57 +0100

    Added new matrix type

--- include/core/Board.h
+++ include/core/Board.h
@@ -1,6 +1,7 @@
 #ifndef SUDOKUBOARD_H
 #define SUDOKUBOARD_H
 
+#include <Matrix.h>
 
 class SudokuBoard
 {
@@ -11,6 +12,9 @@ class SudokuBoard
     protected:
 
     private:
+
+        // TODO set to fields
+        Matrix<int, 3, 3> data;
 };
 
 #endif // SUDOKUBOARD_H
--- src/core/Board.cpp
+++ src/core/Board.cpp
@@ -1,6 +1,6 @@
 #include "Board.h"
 
-SudokuBoard::SudokuBoard()
+SudokuBoard::SudokuBoard() : data()
 {
     //ctor
 }
