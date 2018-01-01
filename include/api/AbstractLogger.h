aeda2b24cb5f68a27049b2d57e005dd1d1e22faf
Miau <tankorn@wp.pl>
2018-01-01 22:06:13 +0100

    Log level details

--- include/api/AbstractLogger.h
+++ include/api/AbstractLogger.h
@@ -2,6 +2,7 @@
 #define ABSTRACTLOGGER_H
 
 #include <memory>
+#include <set>
 #include <string>
 
 enum Log_Level{
@@ -24,6 +25,11 @@ class AbstractLogger {
         virtual void log(const Log_Level lvl, const std::string text) = 0;
 
         void set_log_level(const Log_Level lvl);
+
+    protected:
+        using Log_Level_Set = std::set<Log_Level>;
+
+        Log_Level_Set get_level_set() const;
 
     private:
         Log_Level level;
--- src/api/AbstractLogger.cpp
+++ src/api/AbstractLogger.cpp
@@ -19,3 +19,13 @@ void AbstractLogger::set_log_level(const Log_Level lvl)
 {
     level = lvl;
 }
+
+AbstractLogger::Log_Level_Set AbstractLogger::get_level_set() const
+{
+    return AbstractLogger::Log_Level_Set {
+        Log_Level_Debug,
+        Log_Level_Info,
+        Log_Level_Warning,
+        Log_Level_Error
+    };
+}
